b64f4e842d76515ba7b212ccd466db1f53ff0bba
KODITHUWAKKU ARACHCHIGE SANJU DILSHAN <sanjudilshan554@gmail.com>
2023-08-28 16:22:01 +0530

    fixing navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,61 +1,98 @@
 <template>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <section>
    
-                <div class="navigation">
-                    <nav class="navigationpannel">
-                        <img src="../assets/images/logo.png" alt="lost images" class="logo">
-                        <p class="logoinnovation">SLinnovation</p>
-                    </nav>
-                </div>
-   
-        </section>
+    <header>
+        <div class=""><img src="../assets/images/logo.png" alt="logo" class="logo"></div>
+        <label for="icons" class="icons"><span class="material-symbols-outlined icon">menu</span></label>
+        <ul class="navbar">
+            <li class="link">Home</li>
+            <li class="link">Service</li>
+            <li class="link">Upload</li>
+            <li class="link">Contact</li>
+        </ul>
+        <div class="right">
+            <div class="notification">ICON</div>
+            <div class="user">SANJUDILSHAN</div>
+            <input type="button" value="logout">
+        </div>
+       
+    </header>
+
 </template>
 
 <style>
-.navigationpannel{
-    height: 8vh;
-    width: 100%;
-    top: 0%;
-    left: 0%;
-    background-color: rgba(255, 255, 255, 0.918);
-    display: flex;
-    text-shadow: #0a0a0a 1px 1px 2px 0.1px ;
-    box-shadow: #0a0a0a 1px 1px 6px 0.1px;
-    position: fixed; 
-    z-index: 9999; /* Going content under the navbar */
-    
+body{
+    padding: 0;
+    margin: 0;
 }
-
-.textlogo{
-    padding: 5vh;
-    text-align: center;
-    font-size: small;
-    color: #726a6a;
-}
-
 .logo{
-    margin: 5px 0px 5px 10px ;
-    height: 5vh;
-    width: auto;
-    align-self: flex-start;
+    margin-left: 15px;
+    width:80px;
+    background-color: rgba(0, 255, 255, 0);
+}
+
+.navbar{
+  list-style-type: none;
+   justify-content: center;
+   align-items: center;
+   margin:0;
+   padding: 0;
+}
+
+li{
+    background-color: rgb(197, 206, 214);
+    justify-content: center;
+    padding: 10px;
+}
+
+header{
     display: flex;
+    background-color: rgba(0, 0, 0, 0.1);
+    align-items: center;
+    justify-content:space-between;
+    width: 100%;
+    box-shadow: 1px 1px 5px  black;
+    z-index: 999;
     background-position: fixed;
+    backdrop-filter: blur(50px);
 }
 
-.logoinnovation{
-    margin-top: 2.8vh;
-    align-items: flex-start;
-    font-weight: bold;
-    margin-left: 0%;
-    font-size: auto;
-    
+.right{
+    margin-right: 15px;
+    display: flex;
 }
 
-.navigation{
-    width:100%;
+.icons{
+    display: none;
+}
+/* BREAKPOINTS */
+
+@media (max-width: 992px){
+   
 }
 
+@media(max-width: 693px){
+ 
+    .notification{
+        display: none;
+    }
+   
+    .user{
+        display: none;
+    }
+}
 
+@media (max-width: 500px){
+    .right{
+        display: none;
+    }
+    .navbar{
+        display: none;
+    }
+    .icons{
+        display: block;
+        padding-left: 10%;
+    }
+}
 </style>
 <script></script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,34 @@
 
 <template>
-  <h1 class="display-1">Hello</h1>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <Navbar></Navbar>
+
+      <section>
+        <div class="container">
+          <div class="main">
+
+
+          </div>
+        </div>
+      </section>
+  </body>
+  </html>
 </template>
 
 <script>
+import Navbar from '../components/navbar.vue'
+export default{
+  components:{
+    Navbar
+  }
+}
+
 </script>
 
